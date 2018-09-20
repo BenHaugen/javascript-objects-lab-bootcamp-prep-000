9be25cf67ec200d4ce1747e18740ca509be349fd
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-20 13:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-var newObj = Object.assign({}, object)
+
+
+/*var newObj = Object.assign({}, object)
 function deleteFromObjectByKey(object, key) {
   delete newObj.key
   return newObj
@@ -18,4 +20,4 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
-}
+}*/
